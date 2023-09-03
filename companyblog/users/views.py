b89ce867f55d0cd4fca1c2655e8d384ff55bb7c2
LeonardoMSA <leonardomsazevedo@gmail.com>
2023-09-03 16:31:00 -0300

    Began working on user's account and posts pages

--- companyblog/users/views.py
+++ companyblog/users/views.py
@@ -27,7 +27,7 @@ def register():
 
     return render_template('register.html', form=form)
 
-users.route('login', methods=['GET', 'POST'])
+@users.route('login', methods=['GET', 'POST'])
 def login():
 
     form = LoginForm()
@@ -51,3 +51,38 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('core.index'))
+
+@users.route('/account', methods=['GET','POST'])
+@login_required
+def account():
+
+    form = UpdateUserForm()
+    if form.validate_on_submit():
+
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data, username)
+            current_user.profile_pic = pic
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+
+        flash('Account updated!')
+        return redirect(url_for('users.account'))
+
+    elif request.method == 'Get':
+
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for('static', filename = 'profile_pics/' + current_user.profile_pic)
+    return render_template('account.html', profile_image = profile_image, form = form)
+
+@users.route('/<username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+
+    blog_posts = BlogPost.query.filter_by(author = user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('user_blog_posts.html', blog_posts=blog_posts, user=user)
