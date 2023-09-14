ba68783d949f8a35a9ebbac69cc5f2c1b245d900
LeonardoMSA <leonardomsazevedo@gmail.com>
2023-09-14 11:19:06 -0300

    Adjusted index.html css properties

--- companyblog/core/views.py
+++ companyblog/core/views.py
@@ -7,7 +7,7 @@ core = Blueprint('core', __name__)
 def index():
 
     page = request.args.get('page', 1, type=int)
-    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=6)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=8)
 
     return render_template('index.html', blog_posts=blog_posts)
 
